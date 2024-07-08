2e841bd51d74196948f90e6898d6235e311d6301
scott-parkhill <scott.parkhill@gmail.com>
2024-07-08 11:17:40 -0400

    Fix test_tokenize_input using incorrect function signature

--- tests/test_tokenize_input.py
+++ tests/test_tokenize_input.py
@@ -9,7 +9,7 @@ def multichar_symbols():
 def test_tokenize_input_string_match_all_multichar_symbols(multichar_symbols):
     input_string = "abcde"
     expected_tokens = ["abc", "de"]
-    tokens = tokenize_input_string(None, input_string, multichar_symbols)
+    tokens = tokenize_input_string(input_string, multichar_symbols)
     assert tokens == expected_tokens
 
 # No multichar symbols exist.
@@ -17,21 +17,21 @@ def test_tokenize_input_string_no_multichar_symbols():
     input_string = "abcde"
     multichar_symbols = set()
     expected_tokens = ["a", "b", "c", "d", "e"]
-    tokens = tokenize_input_string(None, input_string, multichar_symbols)
+    tokens = tokenize_input_string(input_string, multichar_symbols)
     assert tokens == expected_tokens
 
 # Part multichar, part not.
 def test_tokenize_input_string_partial_multichar_symbol_match(multichar_symbols):
     input_string = "dabccba"
     expected_tokens = ["d", "abc", "c", "b", "a"]
-    tokens = tokenize_input_string(None, input_string, multichar_symbols)
+    tokens = tokenize_input_string(input_string, multichar_symbols)
     assert tokens == expected_tokens
 
 # Partial multichar match.
 def test_tokenize_input_string_partial_overlapping_match(multichar_symbols):
     input_string = "abcdc"
     expected_tokens = ["abc", "d", "c"]
-    tokens = tokenize_input_string(None, input_string, multichar_symbols)
+    tokens = tokenize_input_string(input_string, multichar_symbols)
     assert tokens == expected_tokens
 
 # Longest multichar first.
@@ -39,12 +39,12 @@ def test_tokenize_input_string_longer_multichar_symbol():
     input_string = "abcdef"
     multichar_symbols = {"abc", "abcdef"}
     expected_tokens = ["abcdef"]
-    tokens = tokenize_input_string(None, input_string, multichar_symbols)
+    tokens = tokenize_input_string(input_string, multichar_symbols)
     assert tokens == expected_tokens
 
 # Empty input.
 def test_tokenize_input_string_empty_input(multichar_symbols):
     input_string = ""
     expected_tokens = []
-    tokens = tokenize_input_string(None, input_string, multichar_symbols)
+    tokens = tokenize_input_string(input_string, multichar_symbols)
     assert tokens == expected_tokens
