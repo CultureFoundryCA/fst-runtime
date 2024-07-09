cc37818f92b5f94dc24a5702273b1987f24b2817
scott-parkhill <scott.parkhill@gmail.com>
2024-07-09 12:50:46 -0400

    Add more tests and docstrings to test_graph_traversal.py

--- tests/test_graph_traversal.py
+++ tests/test_graph_traversal.py
@@ -1,27 +1,70 @@
-import os
-import pytest
-from fst_runtime.directed_graph import DirectedGraph, EPSILON
+'''This module tests that the traversals and queries made to the FST are correct.'''
 
-# Define the path to the data folder
-ATT_DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'data')
+from fst_runtime.fst import Fst, EPSILON
 
-# Generate a list of file paths in the data folder
-att_data_files = [os.path.join(ATT_DATA_FOLDER, filename)
-              for filename
-              in os.listdir(ATT_DATA_FOLDER)
-              if filename.endswith('.att')
-                and os.path.isfile(os.path.join(ATT_DATA_FOLDER, filename))]
+epsilon = [[EPSILON]]
 
-# Define the parameterized test
-# @pytest.mark.parametrize("att_data_file", att_data_files)
-def test_down_traversal(att_data_file = '/home/parkhill/Documents/Coding/fst-runtime/tests/data/fst4.att'):
-    graph = DirectedGraph(att_data_file)
+def test_down_traversal_fst1(att_data_file = '/home/parkhill/Documents/Coding/fst-runtime/tests/data/fst1.att'):
+    '''Tests traveral for fst1.att.'''
 
-    prefix_options = [[EPSILON]]
+    graph = Fst(att_data_file)
+
+    stem1 = 'a'
+    stem2 = 'c'
+    stem3 = 'aaaac'
+
+    stem1_result = graph.down_generation(epsilon, stem1, epsilon)
+    stem2_result = graph.down_generation(epsilon, stem2, epsilon)
+    stem3_result = graph.down_generation(epsilon, stem3, epsilon)
+
+    assert len(stem1_result) == 0
+    assert len(stem2_result) == 1
+    assert len(stem3_result) == 1
+
+    assert stem2_result[0] == 'd'
+    assert stem3_result[0] == 'bbbbd'
+
+def test_down_traversal_fst2(att_data_file = '/home/parkhill/Documents/Coding/fst-runtime/tests/data/fst2.att'):
+    '''Tests traveral for fst2.att.'''
+
+    graph = Fst(att_data_file)
+
+    stem = 'acccccccd'
+    result = graph.down_generation(epsilon, stem, epsilon)
+
+    assert len(result) == 1
+    assert result[0] == 'bccccccce'
+
+def test_down_traversal_fst3(att_data_file = '/home/parkhill/Documents/Coding/fst-runtime/tests/data/fst3.att'):
+    '''Tests traveral for fst3.att.'''
+
+    graph = Fst(att_data_file)
+
+    stem1 = 'aaac'
+    stem2 = 'aaaaaa'
+    stem3 = 'aac'
+
+    stem1_result = graph.down_generation(epsilon, stem1, epsilon)
+    stem2_result = graph.down_generation(epsilon, stem2, epsilon)
+    stem3_result = graph.down_generation(epsilon, stem3, epsilon)
+
+    assert len(stem1_result) == 0
+    assert len(stem2_result) == 1
+    assert len(stem3_result) == 1
+
+    assert stem2_result[0] == stem2
+    assert stem3_result[0] == stem3
+
+
+def test_down_traversal_fst4(att_data_file = '/home/parkhill/Documents/Coding/fst-runtime/tests/data/fst4.att'):
+    '''Tests traveral for fst4.att.'''
+
+    graph = Fst(att_data_file)
+
     stem = 'wal'
     suffix_options = [['+VERB'], ['+INF', '+GER', '+PAST', '+PRES']]
 
-    results = graph.down_generation(prefix_options, stem, suffix_options)
+    results = graph.down_generation(epsilon, stem, suffix_options)
 
     results = set(results)
     expected_results = {'walk', 'walks', 'walked', 'walking'}
