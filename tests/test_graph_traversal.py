44dc576cf67135bc811b6e234921764304010fb0
scott-parkhill <scott.parkhill@gmail.com>
2024-07-10 16:48:48 -0400

    Refactor how epsilon works in test_graph_traversal.py and add test for fst6_waabam.att

--- tests/test_graph_traversal.py
+++ tests/test_graph_traversal.py
@@ -12,7 +12,7 @@ def data_dir():
     """Fixture to provide the path to the data directory."""
     return Path(__file__).parent / "data"
 
-epsilon = [[EPSILON]]
+epsilon = [EPSILON]
 
 def test_down_traversal_fst1(data_dir):
     '''Tests traveral for fst1.att.'''
@@ -23,9 +23,9 @@ def test_down_traversal_fst1(data_dir):
     stem2 = 'c'
     stem3 = 'aaaac'
 
-    stem1_result = graph.down_generation(epsilon, stem1, epsilon)
-    stem2_result = graph.down_generation(epsilon, stem2, epsilon)
-    stem3_result = graph.down_generation(epsilon, stem3, epsilon)
+    stem1_result = graph.down_generation([epsilon], stem1, [epsilon])
+    stem2_result = graph.down_generation([epsilon], stem2, [epsilon])
+    stem3_result = graph.down_generation([epsilon], stem3, [epsilon])
 
     assert len(stem1_result) == 0
     assert len(stem2_result) == 1
@@ -40,7 +40,7 @@ def test_down_traversal_fst2(data_dir):
     graph = Fst(data_dir / 'fst2.att')
 
     lemma = 'acccccccd'
-    result = graph.down_generation(epsilon, lemma, epsilon)
+    result = graph.down_generation([epsilon], lemma, [epsilon])
 
     assert len(result) == 1
     assert result[0] == 'bccccccce'
@@ -54,9 +54,9 @@ def test_down_traversal_fst3(data_dir):
     stem2 = 'aaaaaa'
     stem3 = 'aac'
 
-    stem1_result = graph.down_generation(epsilon, stem1, epsilon)
-    stem2_result = graph.down_generation(epsilon, stem2, epsilon)
-    stem3_result = graph.down_generation(epsilon, stem3, epsilon)
+    stem1_result = graph.down_generation([epsilon], stem1, [epsilon])
+    stem2_result = graph.down_generation([epsilon], stem2, [epsilon])
+    stem3_result = graph.down_generation([epsilon], stem3, [epsilon])
 
     assert len(stem1_result) == 0
     assert len(stem2_result) == 1
@@ -74,9 +74,44 @@ def test_down_traversal_fst4(data_dir):
     lemma = 'wal'
     suffix_options = [['+VERB'], ['+INF', '+GER', '+PAST', '+PRES']]
 
-    results = graph.down_generation(epsilon, lemma, suffix_options)
+    results = graph.down_generation([epsilon], lemma, suffix_options)
 
     results = set(results)
     expected_results = {'walk', 'walks', 'walked', 'walking'}
 
-    assert results == expected_results
+    assert expected_results == results
+
+def test_down_traversal_fst6(data_dir):
+    '''Tests the traversal through the "waabam" FST.'''
+
+    graph = Fst(data_dir / 'fst6_waabam.att')
+
+    prefix_options = [["PVTense/gii+", "PVTense/wii'+"]]
+    lemma = 'waabam'
+
+    suffix_options = [['+VTA'],
+                      ['+Ind'],
+                      ['+Pos'],
+                      ['+Neu'],
+                      ['+1SgSubj'],
+                      ['+2SgObj', '+2PlObj']]
+
+    results = set(graph.down_generation(prefix_options, lemma, suffix_options))
+
+    expected_results = \
+    {
+        "gigiiwaabamin",
+        "gigii-waabamin",
+        "gigii waabamin",
+        "gigiiwaabamininim",
+        "gigii-waabamininim",
+        "gigii waabamininim",
+        "giwii'waabamin",
+        "giwii'-waabamin",
+        "giwii' waabamin",
+        "giwii'waabamininim",
+        "giwii'-waabamininim",
+        "giwii' waabamininim"
+    }
+
+    assert expected_results == results
