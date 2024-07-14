f8d4516a62512b84f2a0d36a98fa973a14fd505b
scott-parkhill <scott.parkhill@gmail.com>
2024-07-14 10:36:38 -0400

    Add module-level docstring to __init__.py for tests module

---
+++ tests/__init__.py
@@ -0,0 +1,26 @@
+"""
+tests
+
+This package contains the unit tests for the `fst_runtime` library.
+
+Modules
+-------
+test_tokenize_input
+    Tests the `tokenize_input_string` function.
+test_graph_creation
+    Tests the graph creation process for the `Fst` class.
+test_traversals
+    Tests the traversals and queries made to the `Fst`.
+
+Fixtures
+--------
+data_dir
+    Provides the path to the data directory used in the tests.
+multichar_symbols
+    Defines the multi-character symbols to use for the tests.
+
+Notes
+-----
+- Each test module contains specific tests for different functionalities of the `fst_runtime` library.
+- The `data_dir` and `multichar_symbols` fixtures are shared among test modules.
+"""
