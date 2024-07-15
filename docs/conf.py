110be7142c3de5867959d92a40703be5c4ade6c4
scott-parkhill <scott.parkhill@gmail.com>
2024-07-15 14:35:26 -0400

    Remove extra period from being shown in sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../fst_runtime'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'fst_runtime'
-copyright = '2024, CultureFoundry Inc.'
+copyright = '2024, CultureFoundry Inc'
 author = 'CultureFoundry Inc., Scott Parkhill'
 release = '0.1.0'
 
