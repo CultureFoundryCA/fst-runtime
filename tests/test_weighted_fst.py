630b98baf2949e2a0670e2720593ba3ad4aa19db
scott-parkhill <scott.parkhill@gmail.com>
2024-08-29 16:59:21 -0400

    Updated weighted graph tests

--- tests/test_weighted_fst.py
+++ tests/test_weighted_fst.py
@@ -1,21 +1,22 @@
 from fst_runtime.fst import Fst
 from fst_runtime.semiring import BooleanSemiring, LogSemiring, ProbabilitySemiring, TropicalSemiring
 
-fst = Fst('tests/data/weighted_graph.att')
-
 def test_weighted_fst_boolean():
     semiring = BooleanSemiring()
-    fst = Fst('tests/data/weighted_graph_boolean.att')
+    fst = Fst('tests/data/weighted_graph_boolean.att', semiring=semiring)
     assert True
 
 def test_weighted_fst_log():
     semiring = LogSemiring()
+    fst = Fst('tests/data/weighted_graph.att', semiring=semiring)
     assert True
 
 def test_weighted_fst_probability():
     semiring = ProbabilitySemiring()
+    fst = Fst('tests/data/weighted_graph.att', semiring=semiring)
     assert True
 
 def test_weighted_fst_tropical():
     semiring = TropicalSemiring()
+    fst = Fst('tests/data/weighted_graph.att', semiring=semiring)
     assert True
